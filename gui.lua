9f1b8dac9760dfd0f133a171952ad4ca81d21fa6
alex <mralexh123@gmail.com>
2019-07-25 16:05:45 -0400

    attempted to convert to tabs
    todo: make info freaking display

--- gui.lua
+++ gui.lua
@@ -2,8 +2,9 @@ local mod_gui = require 'mod-gui'
 
 local function gui_update(player)
     local gui = mod_gui.get_frame_flow(player)
-    local doomsday = gui.doomsday_stats
+    local doomsday = gui.doom_stats
     local PDNC = gui.pdnd_stats
+    local tab = gui.info_tab
 
     -- text = pdnc_extended_status()
     -- game.print(text)
@@ -11,6 +12,9 @@ local function gui_update(player)
         return
     end
     if not PDNC then
+        return
+    end
+    if not tab then
         return
     end
     
@@ -35,72 +39,86 @@ local function toggle_frame(player, button)
     local gui = mod_gui.get_frame_flow(player)
     local doom = gui.doom_stat
     local pdnc = gui.pdnd_stat
-    
-    if doom then
+    local tab = gui.info_tab
+
+    if tab then
         gui.destroy()
         return
     end
 
+    if not tab then
+        tab = gui.add{
+        	name = "info_tab",
+            type = "tabbed-pane",
+        }
+    end
+
 	if not doom then
-        doom = gui.add{
-            type = "button",
+        doom = tab.add{
+            type = "tab",
             name = "doom_stat",
             caption = "Doom Stats",
             tooltip = "Show debug stats for doom",
         }
+        
     end
 
     if not pdnc then
-        pdnc = gui.add{
-            type = "button",
+        pdnc = tab.add{
+            type = "tab",
             name = "pdnd_stat",
             caption = "PDNC Stats",
             tooltip = "Show debug stats for PDNC",
         }
     end 
+    tab1 = tab.add{type="label", caption="Label 1"}
+    tab1 = tab.add_tab(doom, tab1)
+    tab2 = tab.add{type="label", caption="Label 2"}
+    tab2 = tab.add_tab(pdnc, tab2)
 	gui_update(player)
 end
 
 local function doom_stat(player)
-	local gui = mod_gui.get_frame_flow(player)
-	local DoomStat = gui.doomsday_stats
 
-	if DoomStat then
-	    DoomStat.destroy()
-	    return
-	end
+	-- local gui = mod_gui.get_frame_flow(player)
+	-- local DoomStat = gui.doomsday_stats
 
-    DoomStat = gui.add{
-        type = "frame",
-        name = "doomsday_stats",
-        direction = "vertical",
-        caption = "Doomsday stats page",
-        style = mod_gui.frame_style,
-    }
-    DoomStat.style.horizontally_stretchable = false
-	DoomStat.style.vertically_stretchable = false
-	gui_update(player)
+	-- if DoomStat then
+	--     DoomStat.destroy()
+	--     return
+	-- end
+
+ --    DoomStat = tab.add{
+ --        type = "frame",
+ --        name = "doomsday_stats",
+ --        direction = "vertical",
+ --        caption = "Doomsday stats page",
+ --        style = mod_gui.frame_style,
+ --    }
+ --    DoomStat.style.horizontally_stretchable = false
+	-- DoomStat.style.vertically_stretchable = false
+	-- gui_update(player)
 end
 
 local function PDNC_stat(player)
-	local gui = mod_gui.get_frame_flow(player)
-	local PDNCstats = gui.pdnd_stats
+	-- local gui = mod_gui.get_frame_flow(player)
+	-- local PDNCstats = gui.pdnd_stats
 
-	if PDNCstats then
-	    PDNCstats.destroy()
-	    return
-	end
+	-- if PDNCstats then
+	--     PDNCstats.destroy()
+	--     return
+	-- end
 
-    PDNCstats = gui.add{
-        type = "frame",
-        name = "pdnd_stats",
-        direction = "vertical",
-        caption = "PDNC stats pages",
-        style = mod_gui.frame_style,
-    }
-    PDNCstats.style.horizontally_stretchable = false
-	PDNCstats.style.vertically_stretchable = false
-	gui_update(player)
+ --    PDNCstats = gui.add{
+ --        type = "frame",
+ --        name = "pdnd_stats",
+ --        direction = "vertical",
+ --        caption = "PDNC stats pages",
+ --        style = mod_gui.frame_style,
+ --    }
+ --    PDNCstats.style.horizontally_stretchable = false
+	-- PDNCstats.style.vertically_stretchable = false
+	-- gui_update(player)
 end
 
 local function get_sprite_button(player)
@@ -115,9 +133,14 @@ local function get_sprite_button(player)
             tooltip = "Debug stats for doomsday and PDNC",
         }
     end
-
     --add admin check here
     doom.visible = true
+     --    tab1 = tabp.add{type="tab", caption="tab1"}
+    	-- lable1 = tabp.add{type="label", caption="Label 1"}
+    	-- tabpanel = tabp.add_tab(tab1, lable1)
+    	-- tab2 = tabp.add{type="tab", caption="tab2"}
+    	-- lable2 = tabp.add{type="label", caption="Label 2"}
+    	-- tabpanel2 = tabp.add_tab(tab2, lable2)
 end
 
 local function on_gui_click(event)
