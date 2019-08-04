ee6e96934746ca937a1485d747eda9551d1e7f66
zr4g0n <Dupl3xxx@gmail.com>
2019-08-04 15:06:44 +0200

    Reduced data
    Reduced the number of waves down to 8 to show how to spawn each biter type once only. This should be stable and ready for master following addional testing.

--- attack_waves.lua
+++ attack_waves.lua
@@ -14,105 +14,45 @@ global.waves = {{ -- 1
 },	{ -- 2
 	has_happened = false,
 	trigger_tick = global.time_between_waves*3, -- 30min
-	biter_to_spawn = "small-biter",
+	biter_to_spawn = "small-spitter",
 	nodes = 5,
 	group_size = 50
 },	{ -- 3
 	has_happened = false,
 	trigger_tick = global.time_between_waves*4, -- 40min
-	biter_to_spawn = "small-biter",
+	biter_to_spawn = "medium-spitter",
 	nodes = 38,
 	group_size = 9
 },	{ -- 4
 	has_happened = false,
 	trigger_tick = global.time_between_waves*5, -- 50min
-	biter_to_spawn = "medium-spitter",
+	biter_to_spawn = "medium-biter",
 	nodes = 38,
 	group_size = 9
 },	{ -- 5
 	has_happened = false,
 	trigger_tick = global.time_between_waves*6, -- 60min
-	biter_to_spawn = "medium-biter",
+	biter_to_spawn = "big-spitter",
 	nodes = 22,
 	group_size = 18	
 },	{ -- 6
 	has_happened = false,
 	trigger_tick = global.time_between_waves*7, -- 70min
-	biter_to_spawn = "big-spitter",
+	biter_to_spawn = "big-biter",
 	nodes = 18,
 	group_size = 18	
 },	{ -- 7
 	has_happened = false,
-	trigger_tick = global.time_between_waves*8, -- 80min,
-	biter_to_spawn = "big-biter",
+	trigger_tick = global.time_between_waves*9, -- 90min
+	biter_to_spawn = "behemoth-spitter",
 	nodes = 14,
 	group_size = 30	
 },	{ -- 8
 	has_happened = false,
-	trigger_tick = global.time_between_waves*10, -- 100min,
+	trigger_tick = global.time_between_waves*9.1, -- 91min
 	biter_to_spawn = "behemoth-biter",
 	nodes = 8,
 	group_size = 40	
-},	{ -- 9
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12, -- 120min,
-	biter_to_spawn = "behemoth-biter",
-	nodes = 3,
-	group_size = 50	
-}, 	{ -- 10
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.05, -- 120.5min,
-	biter_to_spawn = "behemoth-biter",
-	nodes = 6,
-	group_size = 40	
-}, 	{ -- 11
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.1, -- 121min,
-	biter_to_spawn = "behemoth-spitter",
-	nodes = 9,
-	group_size = 30	
-}, 	{ -- 12
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.15, -- 121.5min,
-	biter_to_spawn = "behemoth-biter",
-	nodes = 12,
-	group_size = 20	
-}, 	{ -- 13
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.2, -- 122min,
-	biter_to_spawn = "behemoth-biter",
-	nodes = 8,
-	group_size = 20	
-}, 	{ -- 14
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.25, -- 122.5min,
-	biter_to_spawn = "behemoth-biter",
-	nodes = 4,
-	group_size = 20	
-}, 	{ -- 15
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.3, -- 123min,
-	biter_to_spawn = "behemoth-spitter",
-	nodes = 3,
-	group_size = 20	
-}, 	{ -- 15
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.35, -- 123.5min,
-	biter_to_spawn = "behemoth-spitter",
-	nodes = 4,
-	group_size = 20	
-}, 	{ -- 16
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.4, -- 124min,
-	biter_to_spawn = "behemoth-spitter",
-	nodes = 3,
-	group_size = 20	
-}, 	{ -- 17
-	has_happened = false,
-	trigger_tick = global.time_between_waves*12.45, -- 124.5min,
-	biter_to_spawn = "behemoth-spitter",
-	nodes = 4,
-	group_size = 20	
 }}
 function biter_poly_path(points)
 	local list_of_commands = {}
@@ -148,14 +88,14 @@ function spawn_biters(biter_type, nodes, group_size)
 	local map_size = {x = 2000, y = 4000} -- maybe get from map-gen settings?
 	local points = {
 		{
-			start = {x = 0 - map_size.x/2.2, y = 0 - map_size.y/3},
-			stop  = {x = map_size.x/2.2,     y = 0 - map_size.y/3}
+			start = {x = 0 - map_size.x/2.1, y = 0 - map_size.y/3},
+			stop  = {x = map_size.x/2.1,     y = 0 - map_size.y/3}
 		},{
-			start = {x = 0 - map_size.x/2.2, y = map_size.y/2.1},
-			stop  = {x = map_size.x/2.2,     y = map_size.y/2.1}
+			start = {x = 0 - map_size.x/2.1, y = map_size.y/2.1},
+			stop  = {x = map_size.x/2.1,     y = map_size.y/2.1}
 		},{
-			start = {x = map_size.x/40,     y = map_size.y/2.2},
-			stop  = {x = 0 - map_size.x/40, y = map_size.y/2.2}
+			start = {x = map_size.x/40,     y = map_size.y/2.3},
+			stop  = {x = 0 - map_size.x/40, y = map_size.y/2.3}
 		}
 	}
 	local step_size = {
