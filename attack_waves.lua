018f554dee28f7bd7ced5f1975d0a27ba2e5337a
zr4g0n <Dupl3xxx@gmail.com>
2019-08-11 23:37:34 +0200

    Cleaning up attack_waves for remote use only
    This includes removing the default data, as this will only be called from a manager module.

--- attack_waves.lua
+++ attack_waves.lua
@@ -1,85 +1,3 @@
--- valid biter names:
--- small-biter     small-spitter
--- medium-biter    medium-spitter
--- big-biter       big-spitter
--- behemoth-biter  behemoth-spitter
--- defaults. Used when attack_waves is run standalone
-local settings = {
-	biter_spawn_radius = 50,
-	startup_message_ticks = 1000,
-	map_size = {x = 2000, y = 2000},
-	surface = 1,
-	spawn_point = {x = 0, y = 0},
-}
-
-local tick_time = {
-	second = 60,
-	minute = 3600,
-	hour = 216000,
-}
-
--- defaults. Used when attack_waves is run standalone
-local lines ={{
-		start = {x = 0 - map_size.x/2.1, y = 0 - map_size.y/3},
-		stop  = {x = map_size.x/2.1,     y = 0 - map_size.y/3}
-	},{
-		start = {x = 0 - map_size.x/2.1, y = map_size.y/2.1},
-		stop  = {x = map_size.x/2.1,     y = map_size.y/2.1}
-	},{
-		start = {x = map_size.x/40,     y = map_size.y/2.3},
-		stop  = {x = 0 - map_size.x/40, y = map_size.y/2.3}
-	}}
-
--- defaults. Used when attack_waves is run standalone
-global.waves = {{
-	has_happened = false,
-	trigger_tick = tick_time.minute*20,
-	biter_to_spawn = "small-biter",
-	nodes = 40,
-	group_size = 6
-},	{
-	has_happened = false,
-	trigger_tick = tick_time.minute*30,
-	biter_to_spawn = "small-spitter",
-	nodes = 5,
-	group_size = 50
-},	{
-	has_happened = false,
-	trigger_tick = tick_time.minute*40,
-	biter_to_spawn = "medium-spitter",
-	nodes = 38,
-	group_size = 9
-},	{
-	has_happened = false,
-	trigger_tick = tick_time.minute*50,
-	biter_to_spawn = "medium-biter",
-	nodes = 38,
-	group_size = 9
-},	{
-	has_happened = false,
-	trigger_tick = tick_time.minute*60,
-	biter_to_spawn = "big-spitter",
-	nodes = 22,
-	group_size = 18	
-},	{
-	has_happened = false,
-	trigger_tick = tick_time.minute*70,
-	biter_to_spawn = "big-biter",
-	nodes = 18,
-	group_size = 18	
-},	{
-	has_happened = false,
-	trigger_tick = tick_time.minute*90,
-	biter_to_spawn = "behemoth-spitter",
-	nodes = 14,
-	group_size = 30	
-},	{
-	has_happened = false,
-	trigger_tick = tick_time.minute*91,
-	biter_to_spawn = "behemoth-biter",
-	nodes = 8,
-	group_size = 40	
-}}
 function biter_poly_path(points)
 	local list_of_commands = {}
 	for i=1, #points do
@@ -139,25 +57,9 @@ function spawn_biters(biter_type, nodes, group_size, points)
 	end
 end
 
-function attack_waves_core()
-	game.forces["player"].set_spawn_position(settings.spawn_point, settings.surface)
-	local tick = game.tick
-	if tick < settings.startup_message_ticks then
-		game.print("Attack waves loaded! Running " .. #global.waves .. " waves. Stand by for first wave!")
-	end
-	for i = 1, #global.waves do
-		if tick >= global.waves[i].trigger_tick and not global.waves[i].has_happened then
-			spawn_biters(global.waves[i].biter_to_spawn, global.waves[i].nodes, global.waves[i].group_size)
-			global.waves[i].has_happened = true
-			if (i ~= #global.waves) then 
-				local time_until_next_wave = global.waves[i + 1].trigger_tick - global.waves[i].trigger_tick
-				game.print("Wave number " .. i .. " has spawned! Next wave in " .. ((time_until_next_wave/60)/60) .. "min!")
-			else
-				game.print("The final wave has spawned!")
-			end
-		end
-	end
-end
+local settings = {}
+local waves = {}
+local lines = {}
 
 function attack_waves_remote_control(data)
 	settings = data.settings
