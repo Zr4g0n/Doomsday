d698fe4316502000a3cbfb4b13dd56ff1c30b782
zr4g0n <Dupl3xxx@gmail.com>
2019-08-21 20:49:26 +0200

    update to sketchbook

--- sketchbook.lua
+++ sketchbook.lua
@@ -30,57 +30,55 @@ test_biter()
 
 
 function test_biter()
-    local groups = game.surfaces[1].create_unit_group({
-        position = {x= -20, y = -20}})
 
-    groups.add_member(game.surfaces[1].create_entity{
-        name = "behemoth-biter",
-        position = game.surfaces[1].find_non_colliding_position("behemoth-biter", {x= -20, y = -20}, 5, 0.3, false)})
-
-    groups.set_command{
-        type = defines.command.compound,
-        structure_type = defines.compound_command.return_last,
-        commands = {
-            {        
-                type = defines.command.attack_area,
-                destination = {x= 20, y = 20},
-                distraction = defines.distraction.none,
-                radius = 3 
-            },
-			{        
-                type = defines.command.attack_area,
-                destination = {x= 20, y = -20},
-                distraction = defines.distraction.none,
-                radius = 3 
-            },
-            {        
-                type = defines.command.attack_area,
-                destination = {x= -20, y = -20},
-                distraction = defines.distraction.none,
-                radius = 3 
-            },
-            {        
-                type = defines.command.attack_area,
-                destination = {x= 20, y = 20},
-                distraction = defines.distraction.none,
-                radius = 3 
-            },
-            {        
-                type = defines.command.attack_area,
-                destination = {x= 20, y = -20},
-                distraction = defines.distraction.none,
-                radius = 3 
-            },
-            {        
-                type = defines.command.attack_area,
-                destination = {x= 0, y = 0},
-                distraction = defines.distraction.none,
-                radius = 3 
-            },
-
+    local range = {min = -150, max = -60}
+    for r = range.min, range.max, 8 do
+        local groups = game.surfaces[1].create_unit_group({
+            position = {x= -200, y = r}})
+        for i = 1, 20 do
+            groups.add_member(game.surfaces[1].create_entity{
+                name = "behemoth-biter",
+                position = game.surfaces[1].find_non_colliding_position("behemoth-biter", {x= -200, y = r}, 50, 1.3, false)})
+        end
+        groups.set_command{
+            type = defines.command.compound,
+            structure_type = defines.compound_command.return_last,
+            commands = {
+                {        
+                    type = defines.command.attack_area,
+                    destination = {x= -150, y = r},
+                    distraction = defines.distraction.none,
+                    radius = 10 
+                },{        
+                    type = defines.command.attack_area,
+                    destination = {x= -100, y = r},
+                    distraction = defines.distraction.none,
+                    radius = 10 
+                },{        
+                    type = defines.command.attack_area,
+                    destination = {x= -30, y = r},
+                    distraction = defines.distraction.none,
+                    radius = 10 
+                }
+            }
         }
-    }
+    end
+    
 end
 test_biter()
 
 
+local f = game.forces['player']
+f.set_gun_speed_modifier('cannon-shell', 100.6)
+f.set_gun_speed_modifier('artillery-shell', 6.0)
+f.set_gun_speed_modifier('bullet', 1.5)
+f.set_gun_speed_modifier('flamethrower', 0)
+f.set_gun_speed_modifier('cannon-shell', 100.6)
+f.set_gun_speed_modifier('grenade', 1)
+f.set_gun_speed_modifier('laser-turret', 22.0)
+f.set_gun_speed_modifier('rocket', 27.2)
+f.set_gun_speed_modifier('shotgun-shell', 0.2)
+
+
+
+/c script.on_nth_tick(15, function() game.player.force.chart_all())end
