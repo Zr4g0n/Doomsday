4a0ea223573ece7b5973e3f10940248a9010c0ce
zr4g0n <Dupl3xxx@gmail.com>
2020-07-23 20:30:22 +0200

    Added some new lines to cheeky text

--- cheeky_text.lua
+++ cheeky_text.lua
@@ -1,8 +1,11 @@
 -- cheeky comments against the player
-global.cheeky_text = {
+-- Don't leave any blank entries!
+global.cheeky_text = { 
 	against_player = {
 		"You're not even worth my time. You're but a bag of meat!",
 		"Oh, what's that? Your flesh is on fire? Weak!",
 		"What that it? Really? There's more, right?",
+		"I know cats that can do better than that!",
+		"Not fair? Ha, you're funny!",
 	},
 }
